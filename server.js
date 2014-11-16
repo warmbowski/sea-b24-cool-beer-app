cfe316de21c43a38c7d54e9adbe1719cfd27e8cd
Paul Laskowski <pelaskowski@gmail.com>
2014-11-15 16:37:15 -0800

    fixed linter and code style problems

--- server.js
+++ server.js
@@ -3,57 +3,54 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var request = require('superagent');
-var fs = require('fs');
-var dbFolder = './db';
+var dbFolder = './db/';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-    app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/public'));
 
-app.get('/', function(req, res, next) {
+app.get('/', function(req, res) {
   res.render('index.html');
 });
 
 var wu = process.env.WU_API_KEY;
-app.post('/', function(req, res, next) {
+app.post('/', function(req, res) {
   var lon = req.body.lon;
   var lat = req.body.lat;
   var wundergUrl = 'http://api.wunderground.com/api/' + wu +
-    '/conditions/q/' + lat + ',' + lon +'.json'
-    console.log(wundergUrl);
-  
+    '/conditions/q/' + lat + ',' + lon + '.json';
+
   request
   .get(wundergUrl)
-  .end(function(err, wundergRes){
+  .end(function(err, wundergRes) {
     if (err) res.sendStatus(500).send('there was a problem');
-    
+
     var parsedData = JSON.parse(wundergRes.text);
     var temp = parsedData.current_observation.temp_f;
-    
+
     if (temp >= 60) {
-      res.json({'msg': 'Too warm', 'beer_list': ''});
+      res.json({msg: 'Too warm', beer_list: ''});
     } else if (temp >= 55) {
-      res.sendfile('./db/warmBeer.json');
+      res.sendfile(dbFolder + 'warmBeer.json');
     } else if (temp >= 50) {
-      res.sendfile('./db/cellarBeer.json');
+      res.sendfile(dbFolder + 'cellarBeer.json');
     } else if (temp >= 45) {
-      res.sendfile('./db/coolBeer.json');
+      res.sendfile(dbFolder + 'coolBeer.json');
     } else if (temp >= 40) {
-      res.sendfile('./db/coldBeer.json');
+      res.sendfile(dbFolder + 'coldBeer.json');
     } else if (temp <= 35) {
-      res.sendFile('./db/veryColdBeer.json');
+      res.sendfile(dbFolder + 'veryColdBeer.json');
     } else {
-      res.json({'msg': 'Too cold', 'beer_list': ''});
+      res.json({msg: 'Too cold', beer_list: ''});
     }
   });
 });
-
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
