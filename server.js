bffb96eed6ce2055df564b7fc2bb4fb633d438e8
Paul Laskowski <pelaskowski@gmail.com>
2014-11-30 11:35:49 -0800

    basic setup for refactoring with browserify

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/build'));
 
 app.get('/', function(req, res) {
   res.render('index.html');
