6450ac672ba2c61a7aa6904524d7db55bd3d76b9
Paul Laskowski <pelaskowski@gmail.com>
2014-11-30 13:32:52 -0800

    added set up build:dev to run before testing, so Travis will build static files before testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,5 +55,5 @@ module.exports = function(grunt) {
 
   grunt.registerTask('build:dev', ['clean:dev', 'browserify:dev', 'copy:dev']);
   grunt.registerTask('test', ['jshint', 'jscs', 'simplemocha']);
-  grunt.registerTask('default', ['test']);
+  grunt.registerTask('default', ['build:dev', 'test']);
 };
