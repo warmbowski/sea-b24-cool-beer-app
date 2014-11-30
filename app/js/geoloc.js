e1eb97593245b78c00b928622d289c6fd85973ee
Paul Laskowski <pelaskowski@gmail.com>
2014-11-30 12:21:44 -0800

    set up grunt bundling commands, cleaned up js code for client and tests, fixed broken css link

--- app/js/geoloc.js
+++ app/js/geoloc.js
@@ -1,44 +1,46 @@
+'use strict';
+var $ = require('jquery');
+
 // geolocation script from https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/Using_geolocation
-  function geoFindMe() {
-    var output = document.getElementById("out");
-    var map = document.getElementById("map");
-    var list = document.getElementById("list");
-    
-    if (!navigator.geolocation){
-      output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-      return;
-    }
+function geoFindMe() {
+  var output = document.getElementById('out');
+  var map = document.getElementById('map');
+  var list = document.getElementById('list');
 
-    function success(position) {
-      var latitude  = position.coords.latitude;
-      var longitude = position.coords.longitude;
-
-      $.ajax({
-        type: "POST",
-        url: '/',
-        data: {lat: latitude, lon: longitude},
-        success: function(data) {
-          console.log(data)
-          output.innerHTML = '<p>' + data.msg + '</p>';
-          list.innerHTML = '<p>You may store ' + data.beer_list + ' on your porch right now.</p>';
-          $("#out").toggleClass('big');
-          
-        }
-      });
-
-      var img = new Image();
-      img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + 
-        longitude + "&zoom=13&size=300x300&sensor=false";
-
-      map.appendChild(img);
-    };
-
-    function error() {
-      output.innerHTML = "Unable to retrieve your location";
-    };
-
-    output.innerHTML = "<p>Thinking…</p>";
-
-    navigator.geolocation.getCurrentPosition(success, error);
+  if (!navigator.geolocation) {
+    output.innerHTML = '<p>Geolocation is not supported by your browser</p>';
+    return;
   }
-  window.onload = geoFindMe;
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    $.ajax({
+      type: 'POST',
+      url: '/',
+      data: {lat: latitude, lon: longitude},
+      success: function(data) {
+        console.log(data);
+        output.innerHTML = '<p>' + data.msg + '</p>';
+        list.innerHTML = '<p>You may store ' + data.beer_list + ' on your porch right now.</p>';
+        $('#out').toggleClass('big');
+      }
+    });
+
+    var img = new Image();
+    img.src = 'http://maps.googleapis.com/maps/api/staticmap?center=' + latitude + ',' +
+      longitude + '&zoom=13&size=300x300&sensor=false';
+
+    map.appendChild(img);
+  }
+
+  function error() {
+    output.innerHTML = 'Unable to retrieve your location';
+  }
+
+  output.innerHTML = '<p>Thinking…</p>';
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+window.onload = geoFindMe;
