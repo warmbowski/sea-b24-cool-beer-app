8195327e319c0f5f7d142577eda94486d79692d6
Paul Laskowski <pelaskowski@gmail.com>
2015-01-09 10:21:19 -0800

    fixing express static route

--- server.js
+++ server.js
@@ -13,11 +13,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-app.use('/', express.static(__dirname + '/build'));
-
-app.get('/', function(req, res) {
-  res.render('index.html');
-});
+app.use(express.static(__dirname + '/build'));
 
 var wu = process.env.WU_API_KEY;
 app.post('/', function(req, res) {
